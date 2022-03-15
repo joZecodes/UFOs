aeed63e7a528fa323bb48b2a2794a86b00e3f6de
joZecodes <zachary.r.dom@gmail.com>
2022-03-15 08:53:54 -0600

    challenge steps 2-5

--- static/js/app.js
+++ static/js/app.js
@@ -29,18 +29,24 @@ var filters = {};
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
-    // 4a. Save the element that was changed as a variable.
+  // 4a. Save the element that was changed as a variable.
+  let changeElement = d3.select(this);
+  // 4b. Save the value that was changed as a variable.
+  let elementValue = changeElement.property("value");
+    console.log(elementValue);
+  // 4c. Save the id of the filter that was changed as a variable.
+  let filterId = changeElement.attr("id");
+    console.log(filterId);
 
-    // 4b. Save the value that was changed as a variable.
-
-    // 4c. Save the id of the filter that was changed as a variable.
-
-  
-    // 5. If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object.
- 
-  
-    // 6. Call function to apply all filters and rebuild the table
+  // 5. If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object.
+  if (elementValue) {
+    filters[filterId] = elementValue;
+  }
+  else {
+    delete filters[filterId];
+  }
+      // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
   }
@@ -60,7 +66,7 @@ function updateFilters() {
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
